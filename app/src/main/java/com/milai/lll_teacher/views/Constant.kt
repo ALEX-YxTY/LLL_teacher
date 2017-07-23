4910c8f83b58e29a48f1b079d94d61a379e9452a
yxty <yxty_01@163.com>
2017-07-23 23:00:48 +0800

    7-23

--- app/src/main/java/com/milai/lll_teacher/views/Constant.kt
+++ app/src/main/java/com/milai/lll_teacher/views/Constant.kt
@@ -10,5 +10,7 @@ object Constant {
     const val LOGOUT: Int = 2
     const val CHANGE_PSW: Int=3    //修改密码 requestCode
     const val REQUEST_STORAGE_PERMISSION: Int = 100 //请求存储权限
+    const val CHOOSE_PICTURE: Int = 4       //选择照片requestCode
+    const val CROP_SMALL_PICTURE: Int = 5   //才叫图片requestCode
 
 }
--- app/src/main/java/com/milai/lll_teacher/views/InformationCommitActivity.kt
+++ app/src/main/java/com/milai/lll_teacher/views/InformationCommitActivity.kt
@@ -1,22 +1,27 @@
 package com.milai.lll_teacher.views
 
+import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.milai.lll_teacher.R
+import com.milai.lll_teacher.custom.util.DialogUtils
+import com.milai.lll_teacher.custom.view.ChooseHeadViewDialog
 import com.tencent.smtt.sdk.ValueCallback
 import com.tencent.smtt.sdk.WebChromeClient
 import com.tencent.smtt.sdk.WebView
 import com.tencent.smtt.sdk.WebViewClient
-import me.iwf.photopicker.PhotoPicker
-import android.support.v4.app.ActivityCompat
-import android.content.pm.PackageManager
-import android.support.v4.content.ContextCompat
-import android.widget.Toast
-import com.milai.lll_teacher.custom.util.DialogUtils
+
+
+
 
 class InformationCommitActivity : AppCompatActivity() {
 
@@ -25,9 +30,9 @@ class InformationCommitActivity : AppCompatActivity() {
     val psw: String by lazy { intent.getStringExtra("psw") }
     val webView: WebView by lazy{findViewById(R.id.wv) as WebView }
     //调起相机的回复
-    var type: Int = 1 //标识是否5.0以上 1-5.0以下  2-5.0及以上
+    var isFromCamera:Boolean = true //标识是否从相机返回
     var mUploadMessage: ValueCallback<Uri>? = null
-    var mUploadMessages: ValueCallback<Array<Uri>>? = null
+    var mUploadMessageAboveLolipop: ValueCallback<Array<Uri>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,48 +73,44 @@ class InformationCommitActivity : AppCompatActivity() {
         settings.allowFileAccess = true// 设置允许访问文件数据
         webView.setWebViewClient(WebViewClient())
         webView.setWebChromeClient(object: WebChromeClient(){
+
+            //above Lollipop
             override fun onShowFileChooser(webView: WebView?, filePathCallback: ValueCallback<Array<Uri>>?
                                            , fileChooserParams: FileChooserParams?): Boolean {
-                Log.d("test","onCLickPicker")
-                type = 2
-                mUploadMessages = filePathCallback
+                Log.d("test","onCLickPicker above Lollipop")
+                mUploadMessageAboveLolipop = filePathCallback
                 choosePicture()
-//                if (mUploadMessage != null) {
-//                    mUploadMessage?.onReceiveValue(null)
-//                }
-//                Log.i("UPFILE", "file chooser params：" + fileChooserParams.toString())
-//                mUploadMessage = filePathCallback
-//                val i = Intent(Intent.ACTION_GET_CONTENT)
-//                i.addCategory(Intent.CATEGORY_OPENABLE)
-//                if (fileChooserParams != null && fileChooserParams.acceptTypes != null
-//                        && fileChooserParams.acceptTypes.length > 0) {
-//                    i.type = fileChooserParams.acceptTypes[0]
-//                } else {
-//                    i.type = "*/*"
-//                }
-//                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE)
-//                return true
                 return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
             }
 
             //android>4.4 使用此方法
             override fun openFileChooser(uploadMsg: ValueCallback<Uri>?, p1: String?, p2: String?) {
                 Log.d("test","onCLickPicker below5.0")
-                type = 1
                 mUploadMessage = uploadMsg
                 choosePicture()
             }
+
         })
         webView.loadUrl("http://lll.domobile.net/Home/Index/addinfo?tel=$tel&verify=$verify&pwd=$psw")
     }
 
     //选择图片 调用PhotoPicker
     private fun choosePicture() {
-        PhotoPicker.builder()
-                .setPhotoCount(1)
-                .setShowCamera(true)
-                .setPreviewEnabled(true)
-                .start(this,PhotoPicker.REQUEST_CODE)
+        ChooseHeadViewDialog(this, R.style.CustomDialog, object : ChooseHeadViewDialog.OnItemClickListener {
+            override fun onClickCamera(view: View, dialog: Dialog) {
+                dialog.dismiss()
+//                cameraWapper()                             //申请相机权限
+            }
+
+            override fun onClickAlbum(view: View, dialog: Dialog) {
+                dialog.dismiss()
+                //调用相册
+                isFromCamera = false
+                val intent = Intent.createChooser(Intent()
+                        .setAction(Intent.ACTION_GET_CONTENT).setType("image/*"), "选择相册")
+                startActivityForResult(intent, Constant.CHOOSE_PICTURE)
+            }
+        }).show()
     }
 
     override fun onBackPressed() {
@@ -120,31 +121,65 @@ class InformationCommitActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (null == mUploadMessage && type == 1) {
-            return
-        }
-        if (null == mUploadMessages && type == 2) {
-            return
-        }
-        if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE && type == 1) {
-            val photos = intent.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS)
-            val result = Uri.parse(photos[0])
-            mUploadMessage?.onReceiveValue(result)
-            mUploadMessage = null
-        } else if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE && type == 2) {
-            val photos = intent.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS)
-            Log.d("test", "photos?: ${null==photos} ${photos.toArray()}")
-            val result = Uri.parse(photos[0])
-            mUploadMessages?.onReceiveValue(arrayOf(result))
-            mUploadMessages = null
-        } else {
-            if (type == 1) {
-                mUploadMessage?.onReceiveValue(null)
-            } else {
-                mUploadMessages?.onReceiveValue(null)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                Constant.CHOOSE_PICTURE ->{
+                    val fileUri = data.data
+                    //对返回的Uri地址进行转换
+//                    fileUri = UriUtils.convertUri(fileUri, this);
+                    onActivityCallBack(isFromCamera,fileUri)
+//                    startPhotoCrop(fileUri) // 开始对图片进行裁剪处理
+                }
+                Constant.CROP_SMALL_PICTURE ->{
+                    //裁剪返回
+                }
             }
         }
+    }
+
+    private fun onActivityCallBack(fromCamera: Boolean, fileUri: Uri?) {
+        if (fromCamera) {
+
+        }
+        if (mUploadMessageAboveLolipop != null && fileUri != null) {
+            val urls = arrayOf(fileUri)
+            mUploadMessageAboveLolipop?.onReceiveValue(urls)
+            mUploadMessageAboveLolipop = null
+        }else if (mUploadMessage != null && fileUri != null) {
+            mUploadMessage?.onReceiveValue(fileUri)
+            mUploadMessage = null
+        } else {
+            mUploadMessage?.onReceiveValue(null)
+            mUploadMessageAboveLolipop?.onReceiveValue(null)
+            Toast.makeText(this, "无法获取数据", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    //裁剪图片Uri
+    fun startPhotoCrop(uri: Uri?) {
+        if (uri == null) {
+            Log.d("tag", "The uri is not exist.")
+            return
+        }
+        val intent = Intent("com.android.camera.action.CROP")
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        intent.setDataAndType(uri, "image/*")
+        // 设置裁剪
+        intent.putExtra("crop", "true")
+        //是否保持比例
+//        intent.putExtra("scale", true)
+        // aspectX aspectY 是宽高的比例
+//        intent.putExtra("aspectX", 1)
+//        intent.putExtra("aspectY", 1)
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 300)
+//        intent.putExtra("outputY", 120)
+        intent.putExtra("return-data", true)
+        startActivityForResult(intent, Constant.CROP_SMALL_PICTURE)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
