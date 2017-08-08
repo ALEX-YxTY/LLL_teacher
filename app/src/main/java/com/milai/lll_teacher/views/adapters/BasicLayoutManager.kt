42352e536e83b2411beee2a086af8e5331bfe36d
yxty_01@163.com <yxty19820729>
2017-08-08 18:36:25 +0800

    8/8 完成页面刷新bug 和上传图片bug  小红点未完成

---
+++ app/src/main/java/com/milai/lll_teacher/views/adapters/BasicLayoutManager.kt
@@ -0,0 +1,14 @@
+package com.milai.lll_teacher.views.adapters
+
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+/**
+ * Created by Administrator on 2017/8/8.
+ *
+ * 主要功能：
+ */
+class BasicLayoutManager(ctx: Context, val listener: LayoutLoadMoreListener):LinearLayoutManager(ctx) {
+//    fun
+}
---
+++ app/src/main/java/com/milai/lll_teacher/views/adapters/LayoutLoadMoreListener.kt
@@ -0,0 +1,11 @@
+package com.milai.lll_teacher.views.adapters
+
+/**
+ * Created by Administrator on 2017/8/8.
+ *
+ * 主要功能：
+ */
+interface LayoutLoadMoreListener {
+
+    fun onLoadMore()
+}
